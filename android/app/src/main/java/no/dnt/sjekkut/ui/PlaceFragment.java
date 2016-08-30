3272a8994b176729ce4f982a5083a26d09253238
Espen Riskedal <espen@cutehacks.com>
2016-08-30 20:43:43 +0200

    Reuse the PlaceAdapter in PlaceFragment
    - we reduce code
    - we get a more symmetric look
    - move to ButterKnife (also, to reduce code)

--- android/app/src/main/java/no/dnt/sjekkut/ui/PlaceFragment.java
+++ android/app/src/main/java/no/dnt/sjekkut/ui/PlaceFragment.java
@@ -22,6 +22,10 @@ import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.squareup.picasso.Picasso;
 
+import java.util.Collections;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import no.dnt.sjekkut.PreferenceUtils;
 import no.dnt.sjekkut.R;
 import no.dnt.sjekkut.Utils;
@@ -51,7 +55,36 @@ public class PlaceFragment extends Fragment implements LocationListener {
     private String mCallbackDescription = "";
     private String mPlaceId = null;
     private Place mPlace = null;
+    private PlaceAdapter mPlaceAdapter;
+    private PlaceAdapter.PlaceViewHolder mPlaceViewHolder;
     private UserCheckins mUserCheckins = null;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.progress)
+    ProgressBar mProgressBar;
+    @BindView(R.id.progress_text)
+    TextView mProgressText;
+    @BindView(R.id.topContainer)
+    ViewGroup mTopContainer;
+    @BindView(R.id.placeContainer)
+    ViewGroup mPlaceContainer;
+    @BindView(R.id.map)
+    ImageView mMap;
+    @BindView(R.id.scale)
+    TextView mScaleBar;
+    @BindView(R.id.description_separator)
+    TextView mDescriptionSeparator;
+    @BindView(R.id.description)
+    TextView mDescriptionText;
+    @BindView(R.id.homepage_separator_title)
+    TextView mHomepageSeparator;
+    @BindView(R.id.homepage_text)
+    TextView mHomepageText;
+    @BindView(R.id.checkin_separator_title)
+    TextView mCheckinSeparator;
+    @BindView(R.id.checkin_text)
+    TextView mCheckinText;
 
     public PlaceFragment() {
         mLocationRequest = LocationRequestUtils.singleShotRequest();
@@ -82,6 +115,7 @@ public class PlaceFragment extends Fragment implements LocationListener {
                 --mCallbackRefCount;
                 if (response.isSuccessful()) {
                     mUserCheckins = response.body();
+                    mPlaceAdapter.setUserCheckins(mUserCheckins);
                 } else {
                     Utils.showToast(getActivity(), getString(R.string.checkin_no_checkins));
                 }
@@ -109,8 +143,11 @@ public class PlaceFragment extends Fragment implements LocationListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View fragmentView = inflater.inflate(R.layout.fragment_place, container, false);
-        Toolbar toolbar = (Toolbar) fragmentView.findViewById(R.id.toolbar);
-        Utils.setupSupportToolbar(getActivity(), toolbar, getString(R.string.screen_place), true);
+        ButterKnife.bind(this, fragmentView);
+        mPlaceAdapter = new PlaceAdapter(getActivity(), null);
+        mPlaceViewHolder = mPlaceAdapter.onCreateViewHolder(mPlaceContainer, 0);
+        mPlaceContainer.addView(mPlaceViewHolder.itemView);
+        Utils.setupSupportToolbar(getActivity(), mToolbar, getString(R.string.screen_place), true);
         setHasOptionsMenu(true);
         return fragmentView;
     }
@@ -167,6 +204,7 @@ public class PlaceFragment extends Fragment implements LocationListener {
     public void onLocationChanged(Location location) {
         --mCallbackRefCount;
         mLocation = location;
+        mPlaceAdapter.setLocation(mLocation);
         if (isAdded()) {
             if (mLocation == null) {
                 Utils.showToast(getActivity(), getString(R.string.error_did_not_find_position));
@@ -188,114 +226,53 @@ public class PlaceFragment extends Fragment implements LocationListener {
         int progressVisibility = callbacksInProgress ? View.VISIBLE : View.INVISIBLE;
         int viewVisibility = callbacksInProgress ? View.INVISIBLE : View.VISIBLE;
 
-        // Progress bar
-
-        ProgressBar progress = (ProgressBar) getView().findViewById(R.id.progress);
-        if (progress != null) {
-            progress.setVisibility(progressVisibility);
+        mProgressBar.setVisibility(progressVisibility);
+        mProgressText.setVisibility(progressVisibility);
+        if (mCallbackDescription != null) {
+            mProgressText.setText(mCallbackDescription);
         }
 
-        TextView text = (TextView) getView().findViewById(R.id.progress_text);
-        if (text != null) {
-            text.setVisibility(progressVisibility);
-            if (mCallbackDescription != null) {
-                text.setText(mCallbackDescription);
-            }
+        mTopContainer.setVisibility(viewVisibility);
+        if (mPlaceAdapter.getItemCount() > 0) {
+            mPlaceAdapter.onBindViewHolder(mPlaceViewHolder, 0);
         }
 
-        // Views
-
-        View topContainer = getView().findViewById(R.id.topContainer);
-        if (topContainer != null) {
-            topContainer.setVisibility(viewVisibility);
+        if (mPlace != null && mPlace.hasLocation()) {
+            Picasso.with(getActivity()).load(Utils.getMapUrl(
+                    Utils.getDisplayWidth(getActivity()),
+                    getResources().getDimensionPixelSize(R.dimen.map_height),
+                    mPlace.getLocation(),
+                    mLocation,
+                    MAP_ZOOMLEVEL)).into(mMap);
+        } else {
+            mMap.setImageResource(0);
         }
 
-        TextView name = (TextView) getView().findViewById(R.id.name);
-        if (name != null) {
-            String placeName = mPlace != null ? mPlace.navn : getString(R.string.unknown_place);
-            name.setText(placeName);
-        }
-        TextView countyAndHeight = (TextView) getView().findViewById(R.id.county);
-        if (countyAndHeight != null) {
-            String county = mPlace != null ? mPlace.fylke : getString(R.string.unknown_county);
-            countyAndHeight.setText(getString(R.string.county, county));
-        }
-        TextView visits = (TextView) getView().findViewById(R.id.visits);
-        if (visits != null && mUserCheckins != null) {
-            int count = mUserCheckins.getNumberOfVisits(mPlaceId);
-            visits.setText(getString(R.string.visits, count));
-        }
-        TextView distance = (TextView) getView().findViewById(R.id.distance);
-        if (distance != null) {
-            String distanceText = mPlace != null && mPlace.hasLocation() && mLocation != null ?
-                    Utils.formatDistance(getActivity(), mLocation.distanceTo(mPlace.getLocation())) : "n/a";
-            distance.setText(getString(R.string.distance_to_you, distanceText));
-        }
-        ImageView image = (ImageView) getView().findViewById(R.id.image);
-        if (image != null) {
-            if (mPlace != null) {
-                Picasso.with(getActivity()).load(mPlace.getImageUrl((int) (Utils.getDisplayWidth(getActivity()) * .25f))).fit().centerCrop().into(image);
-            } else {
-                image.setImageResource(0);
-            }
-        }
-        ImageView map = (ImageView) getView().findViewById(R.id.map);
-        if (map != null) {
-            if (mPlace != null && mPlace.hasLocation()) {
-                Picasso.with(getActivity()).load(Utils.getMapUrl(
-                        Utils.getDisplayWidth(getActivity()),
-                        getResources().getDimensionPixelSize(R.dimen.map_height),
-                        mPlace.getLocation(),
-                        mLocation,
-                        MAP_ZOOMLEVEL)).into(map);
-            } else {
-                map.setImageResource(0);
-            }
-        }
-        TextView scaleBar = (TextView) getView().findViewById(R.id.scale);
-        if (scaleBar != null) {
-            double mapMeters = getResources().getDimensionPixelSize(R.dimen.scalebar_width) * Utils.getMetersPerPixelForGoogleStaticMaps(mLocation, MAP_ZOOMLEVEL);
-            scaleBar.setText(Utils.formatDistance(getActivity(), mapMeters));
-        }
+        double mapMeters = getResources().getDimensionPixelSize(R.dimen.scalebar_width) * Utils.getMetersPerPixelForGoogleStaticMaps(mLocation, MAP_ZOOMLEVEL);
+        mScaleBar.setText(Utils.formatDistance(getActivity(), mapMeters));
 
-        TextView descriptionSeparator = (TextView) getView().findViewById(R.id.description_separator);
-        if (descriptionSeparator != null) {
-            descriptionSeparator.setText(getString(R.string.description_separator));
+        mDescriptionSeparator.setText(getString(R.string.description_separator));
+        String placeDescription = mPlace != null && mPlace.beskrivelse != null ? mPlace.beskrivelse : getString(R.string.unknown_place_description);
+        SpannableString formattedDescription = new SpannableString(Html.fromHtml(placeDescription));
+        mDescriptionText.setText(formattedDescription, TextView.BufferType.SPANNABLE);
+
+        mHomepageSeparator.setText(getString(R.string.homepage_separator));
+        mHomepageText.setMovementMethod(LinkMovementMethod.getInstance());
+        mHomepageText.setText(Html.fromHtml(getString(R.string.more_on_homepage)));
+        String homepageUrl = mPlace != null ? mPlace.getHomePageUrl() : null;
+        String homepageLinkedText = getString(R.string.no_homepage);
+        if (homepageUrl != null) {
+            homepageLinkedText = getString(R.string.more_on_homepage, homepageUrl);
         }
-        TextView descriptionText = (TextView) getView().findViewById(R.id.description);
-        if (descriptionText != null) {
-            String placeDescription = mPlace != null && mPlace.beskrivelse != null ? mPlace.beskrivelse : getString(R.string.unknown_place_description);
-            SpannableString formattedDescription = new SpannableString(Html.fromHtml(placeDescription));
-            descriptionText.setText(formattedDescription, TextView.BufferType.SPANNABLE);
-        }
-        TextView homepageSeparator = (TextView) getView().findViewById(R.id.homepage_separator_title);
-        if (homepageSeparator != null) {
-            homepageSeparator.setText(getString(R.string.homepage_separator));
-        }
-        TextView homepageText = (TextView) getView().findViewById(R.id.homepage_text);
-        if (homepageText != null) {
-            homepageText.setMovementMethod(LinkMovementMethod.getInstance());
-            homepageText.setText(Html.fromHtml(getString(R.string.more_on_homepage)));
-            String homepageUrl = mPlace != null ? mPlace.getHomePageUrl() : null;
-            String homepageLinkedText = getString(R.string.no_homepage);
-            if (homepageUrl != null) {
-                homepageLinkedText = getString(R.string.more_on_homepage, homepageUrl);
-            }
-            homepageText.setText(Html.fromHtml(homepageLinkedText));
-        }
-        TextView checkinSeparator = (TextView) getView().findViewById(R.id.checkin_separator_title);
-        if (checkinSeparator != null) {
-            checkinSeparator.setText(getString(R.string.checkin_separator_title));
-        }
-        TextView checkinText = (TextView) getView().findViewById(R.id.checkin_text);
-        if (checkinText != null) {
-            String checkinDescription = getCheckinStatsString();
-            checkinText.setText(checkinDescription);
-        }
+        mHomepageText.setText(Html.fromHtml(homepageLinkedText));
+
+        mCheckinSeparator.setText(getString(R.string.checkin_separator_title));
+        mCheckinText.setText(getCheckinStatsString());
     }
 
     private void setPlace(Place place) {
         mPlace = place;
+        mPlaceAdapter.setPlaces(Collections.singletonList(place));
         updateView();
     }
 
